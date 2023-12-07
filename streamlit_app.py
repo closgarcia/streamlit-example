db5c43f8d21a63e5e9f6356ab4652d6c17525c6a
Carlos Garcia <112340103+closgarcia@users.noreply.github.com>
2023-12-07 11:30:18 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,47 @@ tracks_df = pd.read_csv('./spotify-dataset-19212020-600k-tracks/tracks.csv')
 # Your existing code for clustering, input_preprocessor, and Music_Recommender functions
 # Fill in your existing code here...
 
+# Example for clustering
+def perform_clustering(data):
+    # Implement your clustering logic here
+    # Example using KMeans
+    kmeans = KMeans(n_clusters=8)
+    clusters = kmeans.fit_predict(data)
+    return clusters
+
+# Example for input preprocessing
+def input_preprocessor(user_songs):
+    # Implement your input preprocessing logic here
+    # Example: Extract relevant features from user input
+    user_features = [song['valence'] for song in user_songs]
+    return user_features
+
+# Example for Music_Recommender function
+def Music_Recommender(user_songs):
+    # Implement your recommendation logic here
+    # Example: Use clustering and input preprocessing
+    # Assume 'tracks_df' is available globally
+
+    # Extract relevant features from the dataset
+    data_for_clustering = tracks_df[features]
+
+    # Perform clustering
+    clusters = perform_clustering(data_for_clustering)
+
+    # Preprocess user input
+    user_features = input_preprocessor(user_songs)
+
+    # Identify the cluster of the user input
+    user_cluster = perform_clustering([user_features])[0]
+
+    # Filter songs in the same cluster as the user input
+    cluster_songs = tracks_df[clusters == user_cluster]
+
+    # Recommend top songs from the cluster
+    recommendations = cluster_songs.head(10)  # Adjust as needed
+    return recommendations
+
+
 # Define features and metadata columns
 features = ['valence', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy',
             'explicit', 'instrumentalness', 'key', 'liveness', 'loudness', 'mode',
