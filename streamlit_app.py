78cba7f7c2095202e2d28c11d2e855be04daced6
Carlos Garcia <112340103+closgarcia@users.noreply.github.com>
2023-12-07 11:09:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,49 @@
-# music_recommender_app.py
-import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
-from sklearn import metrics
-from sklearn.pipeline import Pipeline
-from sklearn.metrics.pairwise import euclidean_distances
-
-# Load your dataset (if not already loaded)
-tracks_df = pd.read_csv('./spotify-dataset-19212020-600k-tracks/tracks.csv')
-
 # Your existing code for clustering, input_preprocessor, and Music_Recommender functions
 
-# Define features and metadata columns
-features = ['valence', 'year', 'acousticness', 'danceability', 'duration_ms', 'energy',
-            'explicit', 'instrumentalness', 'key', 'liveness', 'loudness', 'mode',
-            'popularity', 'speechiness', 'tempo']
-
-metadata_cols = ['year', 'name', 'artists']
-
-# Create a pipeline with StandardScaler and KMeans
-song_cluster_pipeline = Pipeline([
-    ('scaler', StandardScaler()),
-    ('kmeans', KMeans(n_clusters=8, verbose=2))
-], verbose=True)
+# Fit the clustering model to your data
+song_cluster_pipeline.fit(tracks_df[features])
 
 # Function to preprocess input and recommend songs
 def Music_Recommender(song_list, n_songs=10):
-    # Your existing code for recommendation
-# Your existing code...
+    # Assuming input_preprocessor is a function that processes the user's input
+    # and returns a DataFrame with the same columns as your dataset
+    user_input_df = input_preprocessor(song_list)
+
+    # Use the trained pipeline to transform user input and predict clusters
+    user_cluster = song_cluster_pipeline.predict(user_input_df[features])
+
+    # Filter songs that belong to the same cluster as the user
+    cluster_mask = song_cluster_pipeline.predict(tracks_df[features]) == user_cluster
+    cluster_songs = tracks_df[cluster_mask]
+
+    # Sort and select top n_songs based on some criteria (e.g., popularity)
+    recommendations = cluster_songs.sort_values(by='popularity', ascending=False).head(n_songs)
+
+    return recommendations
 
 # Your existing code...
 
 # Streamlit app code
 def main():
-            st.title("Music Recommender App")
-            st.sidebar.header("Input Songs")
+    st.title("Music Recommender App")
+    st.sidebar.header("Input Songs")
 
-            # Get user input for songs
-            song_name = st.sidebar.text_input("Enter Song Name:")
-            song_year = st.sidebar.number_input("Enter Year:", min_value=1920, max_value=2023, step=1)
-                
-            # Create a list of user-input songs
-            user_songs = [{'name': song_name, 'year': int(song_year)}]
-            
-            # Call the Music_Recommender function
-            recommendations = Music_Recommender(user_songs)
-            
-            # Display the recommendations
-            st.subheader("Recommendations:")
-            st.table(recommendations)
+    # Get user input for songs
+    song_name = st.sidebar.text_input("Enter Song Name:")
+    song_year = st.sidebar.number_input("Enter Year:", min_value=1920, max_value=2023, step=1)
 
-# Ensure this block is not indented
+    # Create a list of user-input songs
+    user_songs = [{'name': song_name, 'year': int(song_year)}]
+
+    # Call the Music_Recommender function
+    recommendations = Music_Recommender(user_songs)
+
+    # Display the recommendations
+    st.subheader("Recommendations:")
+    st.table(recommendations)
+
 if __name__ == "__main__":
     main()
+
 
 
